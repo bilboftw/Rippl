d46b7c575c004f65ff51eea021914ebbd07fb3fd
Qix <qix@lavabit.com>
2012-07-18 09:57:46 -0600

    Substituted oprintf for MFC TRACE

--- Natoga Rippl/Rippl/inc/Macros.h
+++ Natoga Rippl/Rippl/inc/Macros.h
@@ -4,16 +4,19 @@
 #ifndef macros_h__
 #define macros_h__
 
+// Includes
+#include "OPrintF.hpp"
+
 // Define windows macro
 #if defined(_WIN32) || defined(_WIN64)
 #define _WINDOWS
 #endif
 
 // Define Tracing Macros
-#define LOGV(fmt, ...) TRACE("[VRB] " fmt "\n", __VA_ARGS__)
-#define LOGD(fmt, ...) TRACE("[DBG] " fmt "\n", __VA_ARGS__)
-#define LOGI(fmt, ...) TRACE("[INF] " fmt "\n", __VA_ARGS__)
-#define LOGW(fmt, ...) TRACE("[WRN]> " fmt "\n", __VA_ARGS__)
-#define LOGE(fmt, ...) TRACE("[ERR]! " fmt "\n", __VA_ARGS__)
+#define LOGV(fmt, ...) oprintf("[VRB] " fmt "\n", __VA_ARGS__)
+#define LOGD(fmt, ...) oprintf("[DBG] " fmt "\n", __VA_ARGS__)
+#define LOGI(fmt, ...) oprintf("[INF] " fmt "\n", __VA_ARGS__)
+#define LOGW(fmt, ...) oprintf("[WRN]> " fmt "\n", __VA_ARGS__)
+#define LOGE(fmt, ...) oprintf("[ERR]! " fmt "\n", __VA_ARGS__)
 
 #endif // macros_h__
