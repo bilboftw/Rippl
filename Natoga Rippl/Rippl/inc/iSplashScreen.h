24a9532055719b0d0ae054a5a40a93007290dbd5
Qix <qix@lavabit.com>
2012-07-18 09:05:41 -0600

    Created splash implementation class declarations

--- Natoga Rippl/Rippl/inc/iSplashScreen.h
+++ Natoga Rippl/Rippl/inc/iSplashScreen.h
@@ -7,6 +7,7 @@
 // Interface Definition
 interface iSplashScreen
 {
+public:
 	virtual void Show() = 0;
 	virtual void Hide() = 0;
 	virtual void UpdateStatus(const char* szString) = 0;
--- Natoga Rippl/Rippl/splash.h
+++ Natoga Rippl/Rippl/splash.h
@@ -4,5 +4,53 @@
 #ifndef splash_h__
 #define splash_h__
 
+// Includes
+#include <windows.h>
+
+#include "iSplashScreen.h"
+
+// Splash State Enum
+typedef enum
+{
+	SPLASH_HIDDEN,
+	SPLASH_SHOWING,
+	SPLASH_VISIBLE,
+	SPLASH_HIDING
+} R_SPLASH_STATE;
+
+// Splash Animation Progression State Enum
+typedef enum
+{
+	SPLASHANIM_FADE,		// The logo fades in
+	SPLASHANIM_SPLIT,		// The logo slides left and reveals the 'console bar'
+	SPLASHANIM_TITLEFADE,	// The rippl title fades in
+	SPLASHANIM_VERFADE,		// The version name/number fades in
+	SPLASHANIM_CONFADE,		// The default console text fades in
+	SPLASHANIM_NATFADE		// The Natoga logo fades in
+} R_SPLASH_ANIM_STATE;
+
+// Splash Class Implementation Declaration
+class Splash : public iSplashScreen
+{
+public:
+	static Splash*					Get();
+	static void						Destroy();
+	
+	void							Show();
+	void							Hide();
+	void							UpdateStatus(const char* szString);
+
+protected:
+	static Splash*					_lpSplash;
+
+	bool							_bAnimating;
+	R_SPLASH_STATE					_ssState;
+	R_SPLASH_ANIM_STATE				_ssAnimState;
+
+	HWND							_hwndWindow;
+private:
+	Splash();
+	~Splash();
+};
 
 #endif // splash_h__
