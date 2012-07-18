f0a2f4ee19a1abfd1e92e384ed91221cf2124c2b
Qix <qix@lavabit.com>
2012-07-18 13:35:29 -0600

    Cleaned up for merge

--- Natoga Rippl/Rippl/winmain.cpp
+++ Natoga Rippl/Rippl/winmain.cpp
@@ -13,23 +13,6 @@
 #include "strmgr.h"
 #include "tween_engine.h"
 
-void onevent(Tween* t, R_TWEEN_CB_MSG msg)
-{
-	switch(msg)
-	{
-	case START:
-		LOGI("Starting tween!");
-		break;
-	case PROGRESS:
-		LOGI("Progress: %u", t->cEasedValue);
-		break;
-	case FINISH:
-		LOGI("Tween finished!");
-		TerminateRippl(0);
-		break;
-	}
-}
-
 /**
  * WinMain Function
  *	Main Windows program entry point
@@ -41,16 +24,6 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 	// DEBUG: Message Box
 	MessageBoxW(NULL, StringMgr::Get()->GetString(R_DEBUG_MSG), StringMgr::Get()->GetString(R_TITLE), MB_OK);
-
-	Tween* twn = new Tween();
-	twn->lDelay = 1000;
-	twn->lDuration = 10000;
-	twn->cbOnEvent = &onevent;
-	twn->rteEase = INOUT;
-
-	TweenEngine::Get()->Add(twn);
-
-	while(true){Sleep(1);}
 	
 	// Release Resources
 	ReleaseResources();
