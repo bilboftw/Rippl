a9024b45418c60fd6ef1c2fb0e07be8b73f289bf
Qix <qix@lavabit.com>
2012-07-18 17:00:00 -0600

    Added cut up PNGs and german version information

--- Natoga Rippl/Rippl/resource.h
+++ Natoga Rippl/Rippl/resource.h
@@ -6,13 +6,17 @@
 #define R_DEBUG_MSG                     103
 #define R_TITLE                         104
 #define R_SPLASH_TITLE                  105
+#define R_PNG_SPLASH_NTGA_LOGO          105
+#define R_PNG_SPLASH_CONBAR             106
+#define R_PNG_SPLASH_MAIN_LOGO          107
+#define IDR_VERSION2                    108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
