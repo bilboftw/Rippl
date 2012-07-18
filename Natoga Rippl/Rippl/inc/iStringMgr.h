6e5c52ac93222ba7deac1249b61b331fe447c010
Qix <qix@lavabit.com>
2012-07-18 09:16:58 -0600

    Created string manager interface

---
+++ Natoga Rippl/Rippl/inc/iStringMgr.h
@@ -0,0 +1,14 @@
+/************************************************************************/
+/* String Manager Interface                                             */
+/************************************************************************/
+#ifndef iStringMgr_h__
+#define iStringMgr_h__
+
+// String Manager Interface
+interface iStringMgr
+{
+public:
+	virtual const char* GetString(DWORD dwStringID, ...) = 0;
+};
+
+#endif // iStringMgr_h__
