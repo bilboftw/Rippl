b505a572f3826abd2c4b9bfeefcbffe0e866b6c4
Qix <qix@lavabit.com>
2012-07-19 17:05:24 -0600

    Declared PNG wrapper class members

--- Natoga Rippl/Rippl/png_wrapper.h
+++ Natoga Rippl/Rippl/png_wrapper.h
@@ -4,5 +4,25 @@
 #ifndef png_wrapper_h__
 #define png_wrapper_h__
 
+// Includes
+#include <windows.h>
+#include <wincodec.h>
+
+// PNG Class
+class PNG
+{
+public:
+	PNG(UINT iRes);
+	~PNG();
+	IStream*				GetIStream();
+	HBITMAP					GetBitmap();
+	IWICBitmapSource*		GetIWICSource();
+protected:
+	UINT					_iRes;
+	IStream*				_lpIStream;
+	HBITMAP					_hbmpHandle;
+	IWICBitmapSource*		_iwicBMPRes;
+	
+};
 
 #endif // png_wrapper_h__
