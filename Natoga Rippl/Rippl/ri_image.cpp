70b9009ecd0e56bc45fbc41c1f8e3d9ffdd610df
Qix <qix@lavabit.com>
2012-07-26 16:07:20 -0600

    Implemented SDL_Image wrapper

--- Natoga Rippl/Rippl/ri_image.cpp
+++ Natoga Rippl/Rippl/ri_image.cpp
@@ -1,3 +1,28 @@
 /************************************************************************/
 /* SDL_Image Wrapper                                                    */
 /************************************************************************/
+
+// Includes
+#include "SDL.h"
+#include "SDL_image.h"
+#include "Macros.h"
+
+#include "ri_image.h"
+
+RIImage::RIImage(const char* szPathTo)
+{
+	// Load image
+	ImageSurface = IMG_Load(szPathTo);
+	if(ImageSurface == NULL)
+	{
+		LOGW("Could not load PNG File: %s", IMG_GetError());
+		assert(false);
+		return;
+	}
+}
+
+RIImage::~RIImage()
+{
+	// Free
+	SDL_FreeSurface(ImageSurface);
+}
--- Natoga Rippl/Rippl/ri_image.h
+++ Natoga Rippl/Rippl/ri_image.h
@@ -1,3 +1,20 @@
 /************************************************************************/
 /* SDL_Image Wrapper                                                    */
 /************************************************************************/
+#ifndef ri_image_h__
+#define ri_image_h__
+
+// Includes
+#include "SDL_image.h"
+
+// SDL Image Wrapper Class
+class RIImage
+{
+public:
+	RIImage(const char* szPathTo);
+	~RIImage();
+
+	SDL_Surface*			ImageSurface;
+};
+
+#endif // ri_image_h__
