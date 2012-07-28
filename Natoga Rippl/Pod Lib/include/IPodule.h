52a1c12663e34aaed2ca19d655d1c28a4b130d4c
Qix <qix@lavabit.com>
2012-07-28 08:21:08 -0600

    Added pod information struct / Removed SDL 1.x lib

--- Natoga Rippl/Pod Lib/include/IPodule.h
+++ Natoga Rippl/Pod Lib/include/IPodule.h
@@ -16,11 +16,25 @@ typedef enum
 	FILETYPE
 } R_POD_TYPE;
 
+// Pod Information struct
+typedef struct  
+{
+	char* Name;				// The name of the Pod
+	char* Version;			// The version of the pod
+	char* Author;			// The programmer/branch
+	char* Manufacturer;		// The manufacturer of the device that is being interfaced
+	R_POD_TYPE Type;		// The type of pod
+} R_POD_INFORMATION;
+
 // Definition
 interface IPodule
 {
 public:
-	const char* 
+	/**
+	 * Returns a struct holding the basic information about
+	 *	the pod
+	 */
+	virtual R_POD_INFORMATION*						GetInfo() = 0;
 };
 
 #endif // IPodule_h__
